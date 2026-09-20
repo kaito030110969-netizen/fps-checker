a7a3f74d3fedfc205b1f6a17b3281cdde424c022
kaikai <331414162+kaito030110969-netizen@users.noreply.github.com>
2026-09-20 09:31:36 +0900

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -27,7 +28,24 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+  <Script
+    src="https://www.googletagmanager.com/gtag/js?id=G-KR91B23H1P"
+    strategy="afterInteractive"
+  />
+
+  <Script id="google-analytics" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-KR91B23H1P');
+    `}
+  </Script>
+
+  {children}
+</body>
     </html>
   );
 }
